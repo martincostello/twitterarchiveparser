2985ed55c5fcc2f2ed408efafe546b848b11f37b
martincostello <martin@martincostello.com>
2016-03-28 13:55:35 +0100

    Add solution
    Add skeleton solution for application.

---
+++ src/TwitterArchiveParser/Program.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.TwitterArchiveParser
+{
+    using System;
+    using System.Reflection;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// A console application that parses the JSON of Twitter archives. This class cannot be inherited.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point to the application.
+        /// </summary>
+        /// <param name="args">The command-line arguments passed to the application.</param>
+        internal static void Main(string[] args)
+        {
+            PrintBanner();
+
+            try
+            {
+                // TODO
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Prints a banner to the console containing assembly and operating system information.
+        /// </summary>
+        private static void PrintBanner()
+        {
+            Assembly assembly = typeof(Program).Assembly;
+            AssemblyName assemblyName = assembly.GetName();
+
+            Console.WriteLine(
+                $@"Twitter Archive Parser - v{assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}
+{assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright}");
+        }
+    }
+}
---
+++ src/TwitterArchiveParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TwitterArchiveParser")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/martincostello/twitterarchiveparser")]
+[assembly: AssemblyCopyright("Copyright © Martin Costello, 2016.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Console application that parses the JSON of Twitter archives.")]
+[assembly: AssemblyProduct("TwitterArchiveParser")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9947dd3c-3c91-45a6-b0df-05b2b7480670")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
